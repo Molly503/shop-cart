a401bd2deea0d8dcd246e19b37de452014efd627
Jie Liang <liangjie_wm@163.com>
2024-07-29 18:56:52 +0800

    array

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import NavbarComponent from "./components/Navbar"
+import { Container } from "react-bootstrap"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Cancel from "./pages/Cancel"
+import Success from "./pages/Success"
+import Store from "./pages/Store"
 
 function App() {
-  return <NavbarComponent></NavbarComponent>
+  return (
+    <Container>
+      <NavbarComponent></NavbarComponent>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Store />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  )
 }
 
 export default App
