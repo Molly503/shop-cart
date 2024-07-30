b3bba88bde0a9c9db31ee23ef755a98ca0dd55b9
Jie Liang <liangjie_wm@163.com>
2024-07-30 17:52:07 +0800

    props

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,25 +1,16 @@
 import { Row, Col } from "react-bootstrap"
+import { productsArray } from "../productsStore"
 
 function Store() {
   return (
     <>
       <h1>Welcome to the store!</h1>
       <Row xs={1} md={3} className="g-4">
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <h1>{product.title}</h1>
+          </Col>
+        ))}
       </Row>
     </>
   )
