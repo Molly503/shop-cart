586d7ac2d04f06b3785f535dc6a74f7356df1585
Jie Liang <liangjie_wm@163.com>
2024-08-04 16:54:57 +0800

    cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,18 @@ function NavbarComponent() {
         <Modal.Header closeButton></Modal.Header>
         <Modal.Title>Shopping Cart</Modal.Title>
         <Modal.Body>
-          <h1>This is the modal body</h1>
+          {productsCount > 0 ? (
+            <>
+              <p>Items in your cart:</p>
+              {cart.items.map((currentProduct, idx) => (
+                <h1>{currentProduct.id}</h1>
+              ))}
+              <h1>Tota:{cart.getTotalCost().toFixed(2)}</h1>
+              <Button variant="success">Purchase items!</Button>
+            </>
+          ) : (
+            <h1>There are no items in your cart!</h1>
+          )}
         </Modal.Body>
       </Modal>
     </>
